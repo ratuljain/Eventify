6b8ed6fa78c5d083e2e6418ef841d81e591b41fe
Ratul <ratul_jain1991@gmail.com>
2016-12-22 13:19:40 +0530

    Added admin end point in url

--- Eventify/urls.py
+++ Eventify/urls.py
@@ -22,6 +22,7 @@
 # ]
 
 from django.conf.urls import url, include
+from django.contrib import admin
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
@@ -43,6 +44,7 @@ router.register(r'users', UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
