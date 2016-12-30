23623b960b0143005d6805d762cb3ef70cd5b991
ratuljain1991 <lol@Ratul.local>
2016-12-30 23:26:54 +0530

    "Changed thresholdLimit=20 for CheckStylePublisher step

--- Eventify/settings.py
+++ Eventify/settings.py
@@ -155,6 +155,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_pylint',
     # 'django_jenkins.tasks.run_jslint',
     # 'django_jenkins.tasks.run_csslint',
     # 'django_jenkins.tasks.run_sloccount'
