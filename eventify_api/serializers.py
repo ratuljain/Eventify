744762097d6064cd98cc0bfdcaaa3db76ae0fb01
ratuljain <ratuljain1991@gmail.com>
2017-01-19 16:17:41 +0530

    Added serializer for EventifyUser, Panelist, Organiser, UserProfileInformation, UserSkill model tables

--- eventify_api/serializers.py
+++ eventify_api/serializers.py
@@ -1,6 +1,51 @@
 from rest_framework import serializers
 
-from eventify_api.models import Event, Venue
+from eventify_api.models import Event, Venue, UserSkill, EventifyUser, UserProfileInformation, Panelist, Organiser, \
+    EventCategory
+
+
+class UserProfileInformationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfileInformation
+        fields = ('id', 'photo_url', 'dob',
+                  'description', 'website_url', 'twitter_url', 'facebook_url',)
+
+
+class UserSkillSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserSkill
+        fields = ('id', 'skill_name', 'skill_description',)
+
+
+class EventifyUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = EventifyUser
+        fields = ('first_name', 'last_name', 'email',
+                  'phone', 'user_profile_information', 'user_skills',)
+
+
+class PanelistSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Panelist
+        fields = ('user',)
+
+
+class OrganiserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Organiser
+        fields = ('user',)
+
+
+class EventCategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = EventCategory
+        fields = ('id', 'category_name',)
 
 
 class VenueSerializer(serializers.HyperlinkedModelSerializer):
--- eventify_api/views.py
+++ eventify_api/views.py
@@ -3,8 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
 
-from eventify_api.models import Venue, Event
-from eventify_api.serializers import VenueSerializer, EventSerializer
+from eventify_api.models import Venue, Event, UserProfileInformation, UserSkill, EventifyUser, Panelist, Organiser, \
+    EventCategory
+from eventify_api.serializers import VenueSerializer, EventSerializer, UserProfileInformationSerializer, \
+    UserSkillSerializer, EventifyUserSerializer, PanelistSerializer, OrganiserSerializer, EventCategorySerializer
 
 
 @api_view(['GET'])
@@ -13,6 +15,66 @@ def api_root(request, format=None):
         'stops': reverse('venue-list', request=request, format=format),
         'events': reverse('event-list', request=request, format=format),
     })
+
+
+class UserProfileInformationList(generics.ListCreateAPIView):
+    queryset = UserProfileInformation.objects.all()
+    serializer_class = UserProfileInformationSerializer
+
+
+class UserProfileInformationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserProfileInformation.objects.all()
+    serializer_class = UserProfileInformationSerializer
+
+
+class UserSkillList(generics.ListCreateAPIView):
+    queryset = UserSkill.objects.all()
+    serializer_class = UserSkillSerializer
+
+
+class UserSkillDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserSkill.objects.all()
+    serializer_class = UserSkillSerializer
+
+
+class EventifyUserList(generics.ListCreateAPIView):
+    queryset = EventifyUser.objects.all()
+    serializer_class = EventifyUserSerializer
+
+
+class EventifyUserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = EventifyUser.objects.all()
+    serializer_class = EventifyUserSerializer
+
+
+class PanelistList(generics.ListCreateAPIView):
+    queryset = Panelist.objects.all()
+    serializer_class = PanelistSerializer
+
+
+class PanelistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Panelist.objects.all()
+    serializer_class = PanelistSerializer
+
+
+class OrganiserList(generics.ListCreateAPIView):
+    queryset = Organiser.objects.all()
+    serializer_class = OrganiserSerializer
+
+
+class OrganiserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Organiser.objects.all()
+    serializer_class = OrganiserSerializer
+
+
+class EventCategoryList(generics.ListCreateAPIView):
+    queryset = EventCategory.objects.all()
+    serializer_class = EventCategorySerializer
+
+
+class EventCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = EventCategory.objects.all()
+    serializer_class = EventCategorySerializer
 
 
 class VenueList(generics.ListCreateAPIView):
