f03867bbb9f25472eac074b83c56fe5e1021583a
ratuljain <ratuljain1991@gmail.com>
2017-01-25 20:35:47 +0530

    Added hyperlink to the EventifyUserSerializer

--- eventify_api/serializers.py
+++ eventify_api/serializers.py
@@ -4,6 +4,16 @@ from eventify_api.models import Event, Venue, UserSkill, EventifyUser, UserProfi
     EventCategory
 
 
+class EventifyUserSerializer(serializers.HyperlinkedModelSerializer):
+    user_profile_information = serializers.HyperlinkedRelatedField(view_name='userprofileinformation-detail', read_only=True)
+    user_skills = serializers.HyperlinkedRelatedField(many=True, view_name='userskills-detail', read_only=True)
+
+    class Meta:
+        model = EventifyUser
+        fields = ('firebase_id', 'first_name',
+                  'last_name', 'email', 'phone', 'user_profile_information', 'user_skills',)
+
+
 class UserProfileInformationSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -17,14 +27,6 @@ class UserSkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserSkill
         fields = ('id', 'skill_name', 'skill_description',)
-
-
-class EventifyUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = EventifyUser
-        fields = ('first_name', 'last_name', 'email',
-                  'phone', 'user_profile_information', 'user_skills',)
 
 
 class PanelistSerializer(serializers.ModelSerializer):
