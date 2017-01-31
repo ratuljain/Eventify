e67e47386dac8f1ad05f739b7906f56a75dabca0
ratuljain <ratuljain1991@gmail.com>
2017-01-31 15:32:03 +0530

    Added post request to submit the firebase token and sending user data if it's valid

---
+++ eventify_api/utils.py
@@ -0,0 +1,18 @@
+import urllib, json
+from jose import jwt
+
+
+def parse_firebase_token(id_token):
+
+    target_audience = "eventifyapp-d5196"
+
+    certificate_url = 'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com'
+
+    response = urllib.urlopen(certificate_url)
+    certs = response.read()
+    certs = json.loads(certs)
+
+    # will throw error if not valid
+    user = jwt.decode(id_token, certs, algorithms='RS256', audience=target_audience)
+
+    return user
--- eventify_api/views.py
+++ eventify_api/views.py
@@ -1,14 +1,19 @@
+import traceback
+
+from jose import JWTError
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+import logging
 
 from eventify_api.models import Venue, Event, UserProfileInformation, UserSkill, EventifyUser, Panelist, Organiser, \
     EventCategory
 from eventify_api.serializers import VenueSerializer, EventSerializer, UserProfileInformationSerializer, \
     UserSkillSerializer, EventifyUserSerializer, PanelistSerializer, OrganiserSerializer, EventCategorySerializer
+from eventify_api.utils import parse_firebase_token
 
 
 @api_view(['GET'])
@@ -100,39 +105,17 @@ class EventDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class FirebaseToken(APIView):
-    def get(self, request, format=None):
-        import urllib, json
-        from jose import jwt
-
-        idtoken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjdkZjNlYmM2NmVkOGJhYjA1YTRjN2U1OTExNDM0YmVjZWU1ZTBkYmMifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZXZlbnRpZnlhcHAtZDUxOTYiLCJuYW1lIjoiSmFuZSBRLiBVc2VyIiwicGljdHVyZSI6Imh0dHBzOi8vZXhhbXBsZS5jb20vamFuZS1xLXVzZXIvcHJvZmlsZS5qcGciLCJhdWQiOiJldmVudGlmeWFwcC1kNTE5NiIsImF1dGhfdGltZSI6MTQ4NTYxNzY3MCwidXNlcl9pZCI6IjlSdVNxRHZsaXVYR0NydENpcWMzbW5wN1J2QTIiLCJzdWIiOiI5UnVTcUR2bGl1WEdDcnRDaXFjM21ucDdSdkEyIiwiaWF0IjoxNDg1NjE3OTA4LCJleHAiOjE0ODU2MjE1MDgsImVtYWlsIjoicmF0dWxqYWluMTk5MUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsicmF0dWxqYWluMTk5MUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.xkUpxRrdm4gHmgTBxjJ5V3ZoXIIXc1ycQKft2yVDre0UP-hUdetBRgeFjR8f1koTd7bnUPHtIckWIOgVOXcXWt5td7Z38DN2vfXAMbSftxYIRmWkizK-C7kANnzspAFE1jzKadU3Jr1gRp_LAYt8GI3C07zUeMdngm-qWm7xifVcce9KQjBUSzv549ka6RaaqAbuhf2f1c75w2CuDeyVMVxOreVSUG2ZZa1m7YnLTi03_Fp4Tl4fN0wIAZ8Zp4MNjHf77Jko_IPzMqXrl7gdcyCzvtDtH00a1_VSqsfVPqTgeyI5lsKfx8QeUT43IgoKKOsKx9CoUY_fKg3TiA3uyg"
-        target_audience = "eventifyapp-d5196"
-
-        certificate_url = 'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com'
-
-        response = urllib.urlopen(certificate_url)
-        certs = response.read()
-        certs = json.loads(certs)
-
-        # will throw error if not valid
-        user = jwt.decode(idtoken, certs, algorithms='RS256', audience=target_audience)
-
-        import sys, time
-
-        for a in range(1, 11):
-            sys.stdout.write('\r {0} files processed.'.format(a))
-            time.sleep(.1)
-
-        print('')
-        sys.stdout.write('\r#####                     (33%)')
-        time.sleep(1)
-        sys.stdout.write('\r#############             (66%)')
-        time.sleep(1)
-        sys.stdout.write('\r#######################   (100%)')
-        print('')
-
-        # print json.loads(user)
-        return Response(user)
+    # def get(self, request, format=None):
+    #     return Response({})
 
     def post(self, request, format=None):
-        print request.data
-        return Response({'key': 'value'}, status=status.HTTP_201_CREATED)
+        id_token = request.data['id_token']
+
+        try:
+            response_body = parse_firebase_token(id_token)
+            request_status = status.HTTP_200_OK
+        except JWTError:
+            response_body = {"id_token": "Signature verification failed."}
+            request_status = status.HTTP_401_UNAUTHORIZED
+
+        return Response(response_body, status=request_status)
